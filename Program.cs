1e67f601e0aae2980d3237ae92c51930a30363df
Leonardo Aparicio <lsilvaaparicio@gmail.com>
2024-09-05 18:44:30 -0300

    Executando métodos

--- Program.cs
+++ Program.cs
@@ -1,8 +1,28 @@
 ﻿using PropriedadesMetodosConstrutores.Models;
 
 Pessoa p1 = new Pessoa();
-
 p1.Nome = "Leo";
-p1.Idade = 20;
+p1.Sobrenome = "Aparicio";
 
-p1.Apresentar();
+Pessoa p2 = new Pessoa();
+p2.Nome = "Maria";
+p2.Sobrenome = "Maio";
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Ingles";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.ListarAlunos();
+
+
+
+
+
+
+// p1.Nome = "Leo";
+// p1.Sobrenome = "Aparicio";
+// p1.Idade = 20;
+//
+// p1.Apresentar();
