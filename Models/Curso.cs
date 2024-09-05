e5c00d4333497415ab2356322a08126c703aa327
Leonardo Aparicio <lsilvaaparicio@gmail.com>
2024-09-05 18:29:02 -0300

    Implementação da classe Curso e criação do Método Adicionar Aluno

---
+++ Models/Curso.cs
@@ -0,0 +1,12 @@
+﻿namespace PropriedadesMetodosConstrutores.Models;
+
+public class Curso
+{
+    public string Nome { get; set; }
+    public List<Pessoa> Alunos { get; set; }
+
+    public void AdicionarAluno(Pessoa aluno)
+    {
+        Alunos.Add(aluno);
+    }
+}
