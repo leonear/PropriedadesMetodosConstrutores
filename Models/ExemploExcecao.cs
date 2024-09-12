baac90d5118ecd8c0c5b632ab4dcabb25e3ae7cc
Leonardo Aparicio <lsilvaaparicio@gmail.com>
2024-09-12 19:57:02 -0300

    Uso do Metodo throw

---
+++ Models/ExemploExcecao.cs
@@ -0,0 +1,31 @@
+﻿namespace PropriedadesMetodosConstrutores.Models;
+
+public class ExemploExcecao
+{
+    public void Metodo1()
+    {
+        try
+        {
+            Metodo2();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Excecao Tratada" + ex.StackTrace);
+        }
+    }
+
+    public void Metodo2()
+    {
+        Metodo3();
+    }
+    public void Metodo3()
+    {
+        Metodo4();
+    }
+    public void Metodo4()
+    {
+        throw new Exception("Ocorreu  uma excecao");
+    }
+
+}
+
